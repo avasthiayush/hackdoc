fc73cb86a6219d8bf64feffbb001bd1defffab42
pprockys <pppandey1120@gmail.com>
2020-06-20 23:56:37 +0530

    user authrntication

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const passport = require('passport');
 
 router.get('/login',(req,res) => {
+    if(req.user){
+        res.redirect('/dashboard-hospital');
+    }
+    else
     res.render('login');
 });
 
@@ -9,5 +13,20 @@ router.get('/signup',(req,res) => {
     res.render('signup');
 });
 
+router.get('/logout',(req,res) => {
+    //res.send('looging out');
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/google',passport.authenticate('google',{
+    scope : ['profile','email']
+}));
+
+// callback function after login
+router.get('/google/redirect',passport.authenticate('google'),(req,res) => {
+    //res.send(req.user);
+    res.redirect('/dashboard-hospital');
+});
 
 module.exports = router;
