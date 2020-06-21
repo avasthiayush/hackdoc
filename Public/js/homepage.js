b1d4155434a78d684b7a80964b590e182164a7ed
pprockys <pppandey1120@gmail.com>
2020-06-21 12:53:26 +0530

    adding js

--- Public/js/homepage.js
+++ Public/js/homepage.js
@@ -11,3 +11,54 @@ const typing = () => {
     }
 }
 typing();
+
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
+/* Getting name of Hospital.*/
+document.getElementById("clicksearch").addEventListener("click",function()
+{
+     let val=document.getElementById("search").value;
+     
+});
+
+
+// updating data
+
+
+// const update = document.querySelector('#update1')
+
+// update.addEventListener('click', _ => {
+//   // Send PUT Request here
+//   fetch('/dashboard/update/beds', {
+//     method: 'put',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(),
+//   })
+
+// })
+
+
+
+
+
+
+
+
+
+
