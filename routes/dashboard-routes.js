f4385c399c65b59af76fb4051b9fde4f65f97290
pprockys <pppandey1120@gmail.com>
2020-06-21 09:51:19 +0530

    adding update function

--- routes/dashboard-routes.js
+++ routes/dashboard-routes.js
@@ -12,6 +12,26 @@ const authCheck = (req,res,next) => {
     }
 };
 
+
+
+router.post('/update/beds',authCheck,(req,res) => {
+    const hos =  Hospital.findOneAndUpdate(req.user,{
+        bedavail1[1]:req.body.totalbeds[1];
+    });
+    //hos.name="xyz";
+    
+    //hos.bedavail2[1]=req.body.totalbeds[1];
+    console.log(hos._conditions);
+
+const doc = hos.save();
+
+    res.render('dashboard-aman',{hospital:req.user});
+    //res.redirect('/dashboard-aman',{hospital:req.user});
+
+    //console.log(req.body);
+});
+
+
 router.get('/',authCheck,(req,res) => {
     res.render('dashboard-hospital',{hospital:req.user},);
 });
