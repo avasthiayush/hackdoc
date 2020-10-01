090832b7589a43578ee5a6350cccf469004efecd
Aman kumar singh <maskmanlucifer@gmail.com>
2020-10-01 15:02:37 +0530

    autocomplete javascript code

--- Public/js/homepage.js
+++ Public/js/homepage.js
@@ -11,49 +11,6 @@ const typing = () => {
     }
 }
 typing();
-
-
-mybutton = document.getElementById("myBtn");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0; 
-}
-
-/* Getting name of Hospital.*/
-document.getElementById("clicksearch").addEventListener("click",function()
-{
-     let val=document.getElementById("search").value;
-     
-});
-
-
-// updating data
-
-
-// const update = document.querySelector('#update1')
-
-// update.addEventListener('click', _ => {
-//   // Send PUT Request here
-//   fetch('/dashboard/update/beds', {
-//     method: 'put',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(),
-//   })
-
-// })
-
-
 function autocomplete(inp, arr) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
@@ -124,7 +81,48 @@ function autocomplete(inp, arr) {
 document.addEventListener("click", function (e) {
     closeAllLists(e.target);
 });
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
+/* Getting name of Hospital.*/
+document.getElementById("clicksearch").addEventListener("click",function()
+{
+     let val=document.getElementById("search").value;
+     
+});
+
+
+// updating data
+
+
+// const update = document.querySelector('#update1')
+
+// update.addEventListener('click', _ => {
+//   // Send PUT Request here
+//   fetch('/dashboard/update/beds', {
+//     method: 'put',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(),
+//   })
+
+// })
+
+
 
 
 
